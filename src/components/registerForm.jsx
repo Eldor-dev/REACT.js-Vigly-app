0297896db8fe48dac1c0e29a8b10d47846484bac
Eldor Azimov <eldorazimov37@gmail.com>
2022-02-11 21:43:10 +0900

    Register Form.

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -23,10 +23,12 @@ class RegisterForm extends Form {
     return (
       <div>
         <h1>Register</h1>
-        {this.renderInput("username", "Username")}
-        {this.renderInput("password", "Password", "password")}
-        {this.renderInput("name", "Name")}
-        {this.renderButton("Register")}
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
       </div>
     );
   }
