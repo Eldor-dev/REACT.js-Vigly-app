a575056eb718fe4167d28d13a6b8b50a3130527a
Eldor Azimov <eldorazimov37@gmail.com>
2022-02-12 13:01:36 +0900

    Search Movies.

---
+++ src/components/common/searchBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBox = ({ value, onChange }) => {
+  return (
+    <input
+      type="text"
+      name="query"
+      placeholder="Search..."
+      value={value}
+      onChange={(e) => onChange(e.currentTarget.value)}
+      className="form-control"
+    />
+  );
+};
+
+export default SearchBox;
